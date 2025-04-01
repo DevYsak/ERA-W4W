0b931a432bfaaa1062788822034d6fdeb6395745
Mayuresh-conexus <mayuresh.mhatre@conexus-ns.com>
2025-04-01 19:11:36 +0530

    merger test

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/hello', [HelloController::class, 'index']);
+<<<<<<< Updated upstream
 Route::get('/about', [HelloController::class, 'about']);
+=======
+Route::get('/user', [UserController::class, 'index']);
+>>>>>>> Stashed changes
